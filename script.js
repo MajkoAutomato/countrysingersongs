cc5a92554d0f02323bd844830994005527ac4a29
MajkoAutomato <126296940+MajkoAutomato@users.noreply.github.com>
2023-04-17 20:12:19 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+
+const search = document.querySelector('.search');
+const countrysingers = document.querySelectorAll('.countrysingers li');
+
+console.log("User typed", countrysingers.length)
+
+const searchengine = e => {
+  const text = e.target.value.toLowerCase();
+
+  console.log("Kieryś gizd pisoł: ", text)
+
+  countrysingers.forEach(el => {
+
+      const task = el.textContent;
+
+      if   (task.toLowerCase().indexOf(text) !== -1) {
+          el.style.display = 'block'
+      }    else  {
+          el.style.display = 'none'
+      }
+
+  })
+};
+
+  search.addEventListener('keyup', searchengine) 
